bb1d6d3dce835a4d1167eba959e86d3300ff9266
meghavardhan-udutha <uduthameghavardhan99@gmail.com>
2025-07-14 19:04:34 +0530

    Moved requirements.txt to root

--- backend/frontend/script.js
+++ backend/frontend/script.js
@@ -1,5 +1,5 @@
 // const API_URL = 'http://127.0.0.1:5000/problems';
-const API_URL = 'https://problems-tracker.onrender.com/problems';
+const API_URL = 'https://problems-tracker.onrender.com/problems'; //https://problems-tracker.onrender.com
 let currentFilter = 'all';
 let allProblems = [];
 let difficultyFilter = 'all';
